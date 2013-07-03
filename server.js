c372f30ff282e4b1f305e134756aa7d9ce4d0504
Raphael Villas <raphael.villas@gmail.com>
2013-07-03 17:37:04 -0500

    Added Jade and Stylus

--- server.js
+++ server.js
@@ -1,6 +1,22 @@
-var express = require('express');
+var express = require('express')
+  , stylus = require('stylus')
+  , nib = require('nib');
+
 var app = express();
 app.set('view engine', 'jade');
+app.use("/stylesheets", express.static(__dirname + '/stylesheets'));
+
+function compile(str, path) {
+  return stylus(str)
+    .set('filename', path)
+    .set('compress', true)
+    .use(nib());
+}
+
+app.use(stylus.middleware({
+    src: __dirname
+  , compile: compile
+}));
 
 app.get('/', function(req, res){
   res.setHeader('Content-Type', 'text/html');
